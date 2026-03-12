184f5b2d280cb9b13f549e5184f3f5255477191c
Thomas Christory <tchristory@partner.auchan.fr>
2026-03-12 18:30:18 +0100

    Add proxy list table

---
+++ netbox_proxies/tables.py
@@ -0,0 +1,29 @@
+import django_tables2 as tables
+from netbox.tables import NetBoxTable, ChoiceFieldColumn
+
+from .models import Proxy
+
+
+class ProxyTable(NetBoxTable):
+    name = tables.Column(linkify=True)
+    protocol = ChoiceFieldColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = Proxy
+        fields = (
+            "pk",
+            "id",
+            "name",
+            "protocol",
+            "server",
+            "port",
+            "description",
+        )
+        default_columns = (
+            "pk",
+            "name",
+            "protocol",
+            "server",
+            "port",
+            "description",
+        )
