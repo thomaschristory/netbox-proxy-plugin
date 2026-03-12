3efc5c5698542e99c0a65789b7fdecec69b987bb
Thomas Christory <tchristory@partner.auchan.fr>
2026-03-12 18:30:18 +0100

    Add proxy forms for create, edit, import, and filtering

---
+++ netbox_proxies/forms.py
@@ -0,0 +1,41 @@
+from netbox.forms import NetBoxModelForm, NetBoxModelFilterSetForm, NetBoxModelImportForm
+from utilities.forms.fields import CommentField
+
+from .models import Proxy, ProxyProtocolChoices
+
+
+class ProxyForm(NetBoxModelForm):
+    comments = CommentField()
+
+    class Meta:
+        model = Proxy
+        fields = (
+            "name",
+            "protocol",
+            "server",
+            "port",
+            "username",
+            "password",
+            "description",
+            "tags",
+            "comments",
+        )
+
+
+class ProxyImportForm(NetBoxModelImportForm):
+    class Meta:
+        model = Proxy
+        fields = (
+            "name",
+            "protocol",
+            "server",
+            "port",
+            "username",
+            "password",
+            "description",
+        )
+
+
+class ProxyFilterForm(NetBoxModelFilterSetForm):
+    model = Proxy
+    protocol = ProxyProtocolChoices
