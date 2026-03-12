d06198697e2fb685b8fd7a688ab4377870affcf5
Thomas Christory <tchristory@partner.auchan.fr>
2026-03-12 21:03:17 +0100

    Add __version__ at module level (closes #2)

--- netbox_proxy_plugin/__init__.py
+++ netbox_proxy_plugin/__init__.py
@@ -1,11 +1,13 @@
 from netbox.plugins import PluginConfig
 
+__version__ = "0.1.0"
+
 
 class NetboxProxyPluginConfig(PluginConfig):
     name = "netbox_proxy_plugin"
     verbose_name = "Proxies"
     description = "Manage HTTP proxy configurations for NetBox"
-    version = "0.1.0"
+    version = __version__
     author = "thomaschristory"
     base_url = "proxies"
     min_version = "4.2.0"
